1ead94bdb8d88c5a861045172cac72df8b4c6643
tuaigo <tiago.olivei0801@gmail.com>
2024-05-02 20:38:52 -0300

    inicio codigo

--- DesafioProjeto/src/ContaTerminal.java
+++ DesafioProjeto/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private int numeroConta;
+    private double saldo;
+
+    public ContaTerminal(int numeroConta, double saldo){
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
     }
+    public void depositar(double valor){
+        saldo += valor;
+        System.out.println("Deposito de " + valor + "realizado com sucesso");
+    }
+    public void sacar(double valor){
+        if(saldo <= valor){
+            saldo -= valor;
+            System.err.println("Saque de "+ valor + "realizado com sucesso");
+            }else{
+            System.err.println("Saldo insuficiente");
+        }
+        public double getSaldo(){
+            return saldo;
+        }
+        public static void main(String[]args){
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite o numero da conta: ");
+            int numeroConta = scanner.nextInt();
+
+            System.out.println("Digite o saldo da conta: ");
+            double saldo = scanner.nextDouble();
+            
+        }
+
+    }
+        
 }
