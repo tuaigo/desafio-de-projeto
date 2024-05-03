4f124f53d74a8b088749c63c8d5d81426823c78b
tuaigo <tiago.olivei0801@gmail.com>
2024-05-02 23:12:49 -0300

    codigo feito!

--- DesafioProjeto/src/ContaTerminal.java
+++ DesafioProjeto/src/ContaTerminal.java
@@ -1,43 +1,48 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
+    private static boolean ContaTerminal;
     private int numeroConta;
+    private String agencia;
+    private String nomeCliente;
     private double saldo;
 
-    public ContaTerminal(int numeroConta, double saldo){
+    public ContaTerminal(int numeroConta, String agencia, String nomeCliente, double saldo){
         this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
         this.saldo = saldo;
     }
-
     public void depositar(double valor){
         saldo += valor;
-        System.out.println("Deposito de " + valor + "realizado com sucesso");
+        System.out.println("Deposito de "+valor+"feito com sucesso!");
     }
-
-    public double sacar(double valor){
-        if(saldo <= valor){
+    public double sacar (double valor){
+        if(saldo < valor){
             saldo -= valor;
-            System.out.println("Saque de "+ valor + "realizado com sucesso");
-        } else {
-            System.out.println("Saldo insuficiente");
+            System.out.println("Saque de "+valor+"feito com sucesso!");
         }
-        return saldo;
+        return valor;
     }
-
+    
     public double getSaldo(){
         return saldo;
     }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite sua conta");
+        int numeroConta = scan.nextInt();
+        System.out.println("Digite sua agencia");
+        String agencia = scan.next();
+        System.out.println("Digite seu nome");
+        String nomeCliente = scan.next();
+        System.out.println("Digite seu saldo");
+        double saldo = scan.nextDouble();
 
-        System.out.println("Digite o numero da conta: ");
-        int numeroConta = scanner.nextInt();
-
-        System.out.println("Digite o saldo da conta: ");
-        double saldo = scanner.nextDouble();
-
-        ContaTerminal conta = new ContaTerminal(numeroConta, saldo);
-
+        ContaTerminal conta = new ContaTerminal(numeroConta, agencia, nomeCliente, saldo);
+        if (ContaTerminal = true) {
+            System.out.println("Ola cliente "+nomeCliente+"Obrigado por criar uma conta no nosso banco, sua agencia é: "+agencia+"conta: "+numeroConta+"e seu saldo: "+saldo+"já está disponivel para saque.");
+            
+        }
     }
 }
